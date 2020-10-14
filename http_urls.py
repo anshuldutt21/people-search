5a5a7722424a15b12095d43b444b9c24703eb32a
anshuldutt21 <anshuldutt21@gmail.com>
2020-10-15 00:26:36 +0530

    remove bhawan search view

--- http_urls.py
+++ http_urls.py
@@ -5,7 +5,6 @@ from rest_framework import routers
 from people_search.views.student_search import StudentSearch
 from people_search.views.faculty_search import FacultySearch
 from people_search.views.interest_search import InterestSearch
-from people_search.views.bhawan_search import BhawanSearch
 
 app_name = 'people_search'
 router = routers.DefaultRouter()
@@ -13,7 +12,7 @@ router = routers.DefaultRouter()
 router.register(r'student_search',StudentSearch,basename="student_search")
 router.register(r'faculty_search',FacultySearch,basename="faculty_search")
 router.register(r'interest_search',InterestSearch, basename="interest_search")
-router.register(r'student/bhawan_search',BhawanSearch, basename="bhawan_search")
+
 
 urlpatterns = [
 
